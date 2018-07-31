6df3352cb202d69c3869e2588e609488f1e139d6
Ali Saleh <alisheikh1114@gmail.com>
2018-07-31 16:04:30 +0500

    Updated with conditional rendering

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -35,8 +35,34 @@ class TopNavigation extends Component {
               </li>
             </ul>
 
-            <Link className="btn btn-primary btn-sm login_btn" to="/login">Login</Link>
-            <Link className="btn btn-success btn-sm signup_btn" to="/signup">Sign Up</Link>
+            {this.props.logged_in ?
+              <React.Fragment>
+                <a href="/" style={{marginRight: 15, position: 'relative'}} id="check_messages" title="Inbox"><i className="fa fa-inbox fa-lg" aria-hidden="true" /><span id="messages_count" /></a>
+                <a href="/" style={{marginRight: 15, position: 'relative'}} id="check_activities" title="Notifications"><i className="fa fa-bell-o fa-lg" aria-hidden="true" /><span id="activities_count" /></a>
+                <a href="/" style={{marginRight: 15, position: 'relative'}} title="Post a new subject"><i className="fa fa-pencil fa-lg" aria-hidden="true" /></a>
+                <div className="dropdown">
+                  <a href="#" role="button" id="user_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Profile and settings"><img src={this.props.user_details.profile_picture} style={{width: 36, height: 36, borderRadius: 50}} /></a>
+                  <div className="dropdown-menu" aria-labelledby="user_dropdown" style={{right: 0, left: 'auto'}}>
+                    <a className="dropdown-item font-weight-bold" href="/">{this.props.user_details.screen_name}</a>
+                    <div className="dropdown-divider" />
+                    <a className="dropdown-item" href="/">View profile</a>
+                    <a className="dropdown-item" href="/">Following</a>
+                    <a className="dropdown-item" href="/">Subscriptions</a>
+                    <div className="dropdown-divider" />
+                    <a className="dropdown-item" href="/">Create Board</a>
+                    <a className="dropdown-item" href="/">Your Boards</a>
+                    <div className="dropdown-divider" />
+                    <a className="dropdown-item" href="/">Account settings</a>
+                    <a className="dropdown-item font-weight-light" href="/">Send Feedback</a>
+                    <a className="dropdown-item pointer_link" onClick={this.props.handle_logout}>Sign out</a>
+                  </div>
+                </div>
+              </React.Fragment> :
+              <React.Fragment>
+                <Link className="btn btn-primary btn-sm login_btn" to="/login">Login</Link>
+                <Link className="btn btn-success btn-sm signup_btn" to="/signup">Sign Up</Link>
+              </React.Fragment>
+            }
 
           </div>
         </div>
