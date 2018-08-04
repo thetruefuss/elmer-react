ef3db8170f4061fef14635064ecbf287337a24bc
Ali Saleh <alisheikh1114@gmail.com>
2018-08-04 16:27:25 +0500

    Added stylesheet for ActiveThreads

--- src/components/ActiveThreads.js
+++ src/components/ActiveThreads.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './ActiveThreads.css';
 import ReactPlaceholder from 'react-placeholder';
 import "react-placeholder/lib/reactPlaceholder.css";
 
@@ -43,14 +44,14 @@ class ActiveThreads extends Component {
       <React.Fragment>
         {this.state.logged_in ?
           <div className="card border-light">
-            <div className="card-header text-center font-weight-bold" style={{color: '#607d8b', borderBottom: '2px solid #1f89de', display: 'inline-block', fontSize: 16, fontWeight: 900, letterSpacing: '-.4px', margin: '3px 0', paddingBottom: 0, textTransform: 'uppercase'}}>
+            <div className="card-header text-center font-weight-bold active-threads-header">
               Active Threads
             </div>
             <ul className="list-group list-group-flush">
               {threads.length > 0 ? threads.map((thread, index) => {
                     return (
                       <ReactPlaceholder showLoadingAnimation type="textRow" delay={2000} ready={this.state.ready}>
-                        <li className="list-group-item" style={{fontSize: 14}} key={`active-threads-key ${index}`}>
+                        <li className="list-group-item threads-font-size" key={`active-threads-key ${index}`}>
                           <Link to={`/s/${thread.board_slug}/${thread.slug}`} class="card-link">{thread.title}</Link>
                         </li>
                       </ReactPlaceholder>
