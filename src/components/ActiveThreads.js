d5870016bc08791d4615ad648c2926ddead619ff
Ali Saleh <alisheikh1114@gmail.com>
2018-08-04 16:18:15 +0500

    Updated for logged out users

--- src/components/ActiveThreads.js
+++ src/components/ActiveThreads.js
@@ -8,6 +8,7 @@ class ActiveThreads extends Component {
     super(props);
 
     this.state = {
+      logged_in: localStorage.getItem('token') ? true : false,
       threads: [
         "This is just",
         "a mock data",
@@ -20,16 +21,19 @@ class ActiveThreads extends Component {
   }
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/frontboard/subjects/active_threads/', {
-      headers: {
-        Authorization: `JWT ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json'
-      },
-    })
-      .then(res => res.json())
-      .then(json => {
-        this.setState({ threads: json, ready: true });
-      });
+    const url = 'http://127.0.0.1:8000/api/frontboard/subjects/active_threads/';
+    if (this.state.logged_in) {
+      fetch(url, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        },
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({ threads: json, ready: true });
+        });
+    }
   }
 
   render() {
@@ -37,21 +41,23 @@ class ActiveThreads extends Component {
 
     return (
       <React.Fragment>
-        <br />
-        <div className="card border-light">
-          <div className="card-header text-center font-weight-bold" style={{color: '#607d8b', borderBottom: '2px solid #1f89de', display: 'inline-block', fontSize: 16, fontWeight: 900, letterSpacing: '-.4px', margin: '3px 0', paddingBottom: 0, textTransform: 'uppercase'}}>
-            Active Threads
-          </div>
-          <ul className="list-group list-group-flush">
-            {threads.length > 0 ? threads.map((thread, index) => {
-                  return (
-                    <ReactPlaceholder showLoadingAnimation type="textRow" delay={2000} ready={this.state.ready}>
-                      <li className="list-group-item" style={{fontSize: 14}} key={`active-threads-key ${index}`}><Link to={`/s/${thread.board_slug}/${thread.slug}`} class="card-link">{thread.title}</Link></li>
-                    </ReactPlaceholder>
-                  );
-            }) : <p>No threads Found</p>}
-          </ul>
-        </div>
+        {this.state.logged_in ?
+          <div className="card border-light">
+            <div className="card-header text-center font-weight-bold" style={{color: '#607d8b', borderBottom: '2px solid #1f89de', display: 'inline-block', fontSize: 16, fontWeight: 900, letterSpacing: '-.4px', margin: '3px 0', paddingBottom: 0, textTransform: 'uppercase'}}>
+              Active Threads
+            </div>
+            <ul className="list-group list-group-flush">
+              {threads.length > 0 ? threads.map((thread, index) => {
+                    return (
+                      <ReactPlaceholder showLoadingAnimation type="textRow" delay={2000} ready={this.state.ready}>
+                        <li className="list-group-item" style={{fontSize: 14}} key={`active-threads-key ${index}`}>
+                          <Link to={`/s/${thread.board_slug}/${thread.slug}`} class="card-link">{thread.title}</Link>
+                        </li>
+                      </ReactPlaceholder>
+                    );
+              }) : <p>No Threads Found</p>}
+            </ul>
+          </div> : ""}
       </React.Fragment>
     );
   }
