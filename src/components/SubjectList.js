cd7cfaff2dc6cf2086cd4102dda289710705718e
Ali Saleh <alisheikh1114@gmail.com>
2018-08-16 02:20:40 +0500

    Updated component to use state from redux store

--- src/components/SubjectList.js
+++ src/components/SubjectList.js
@@ -1,66 +1,27 @@
 import React, { Component } from 'react';
-import SubjectDetail from './SubjectDetail';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ReactPlaceholder from 'react-placeholder';
 import "react-placeholder/lib/reactPlaceholder.css";
 import SubjectPlaceholder from './SubjectPlaceholder';
+import SubjectDetail from './SubjectDetail';
+import { fetchSubjects } from '../actions/subjects';
 
 class SubjectList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      logged_in: localStorage.getItem('token') ? true : false,
-      subjects: [
-        "This is just a mock data for subjects.",
-        "Just to show content placeholder twice."
-      ],
-      ready: false
-    };
-  }
-
   componentDidMount() {
     const { url } = this.props;
-    if (this.state.logged_in) {
-      fetch(url, {
-        headers: {
-          Authorization: `JWT ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json'
-        },
-      })
-        .then(res => res.json())
-        .then(json => {
-          if (json.results.length > 0) {
-            this.setState({ subjects: json.results, ready: true });
-          } else {
-            this.setState({ subjects: [], ready: true });
-          }
-        });
-    } else {
-      fetch(url, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-        .then(res => res.json())
-        .then(json => {
-          if (json.results.length > 0) {
-            this.setState({ subjects: json.results, ready: true });
-          } else {
-            this.setState({ subjects: [], ready: true });
-          }
-        });
-    }
+    this.props.fetchSubjects(url);
   }
 
   render() {
-    const { subjects } = this.state;
+    const { subjects } = this.props;
 
     return (
       <React.Fragment>
         {subjects.length > 0 ? subjects.map((subject, index) => {
               return (
-                <ReactPlaceholder showLoadingAnimation delay={2000} ready={this.state.ready} customPlaceholder={SubjectPlaceholder}>
-                  <SubjectDetail subject={subject} key={`subject-list-key ${index}`} />
+                <ReactPlaceholder showLoadingAnimation delay={2000} ready={this.props.ready} customPlaceholder={SubjectPlaceholder}>
+                  <SubjectDetail subject={subject} key={index} />
                 </ReactPlaceholder>
               );
         }) : <p>No Posts Found</p>}
@@ -69,4 +30,16 @@ class SubjectList extends Component {
   }
 }
 
-export default SubjectList;
+SubjectList.propTypes = {
+  fetchSubjects: PropTypes.func.isRequired,
+  subjects: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  subjects: state.subjectReducer.subjects,
+  ready: state.subjectReducer.ready
+});
+
+export default connect(mapStateToProps, { fetchSubjects })(SubjectList);
