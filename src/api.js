5a6f0a1c372f63b6d72af5e539fdf3a4c2b554a5
Ali Saleh <alisheikh1114@gmail.com>
2018-08-22 18:31:01 +0500

    API call for user login

--- src/api.js
+++ src/api.js
@@ -3,5 +3,9 @@ import axios from "axios";
 export default {
   subjects: {
     getAll: url => axios.get(url).then(res => res.data.results)
+  },
+  user: {
+    login: credentials =>
+      axios.post("http://127.0.0.1:8000/api/users/login/", credentials).then(res => res.data)
   }
 };
