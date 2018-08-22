f2b41701bf9573876aa8f5c633c05151ff08f709
Ali Saleh <alisheikh1114@gmail.com>
2018-08-22 18:34:36 +0500

    Connected with Redux

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import axios from 'axios';
+import * as actions from "../actions/auth";
 import SearchInput from './SearchInput';
 import './TopNavigation.css';
 import elmer_logo from '../images/elmer_logo.png';
 
 class TopNavigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_details: {}
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.isAuthenticated) {
+      axios.get("http://127.0.0.1:8000/api/users/current_user/").then(res => this.setState({user_details: res.data}))
+    }
+  }
+  
   render() {
     return (
       <nav className="navbar sticky-top navbar-expand-lg navbar-expand-md navbar-light extra_navbar_styling">
@@ -31,19 +48,19 @@ class TopNavigation extends Component {
               </li>
             </ul>
 
-            {this.props.logged_in ?
+            {this.props.isAuthenticated ?
               <React.Fragment>
                 <Link to="/messages" style={{marginRight: 15, position: 'relative'}} id="check_messages" title="Inbox"><i className="fa fa-inbox fa-lg" aria-hidden="true" /><span id="messages_count" /></Link>
                 <Link to="/activities" style={{marginRight: 15, position: 'relative'}} id="check_activities" title="Notifications"><i className="fa fa-bell-o fa-lg" aria-hidden="true" /><span id="activities_count" /></Link>
                 <Link to="/new_post" style={{marginRight: 15, position: 'relative'}} title="Post a new subject"><i className="fa fa-pencil fa-lg" aria-hidden="true" /></Link>
                 <div className="dropdown">
                   <a href="#" role="button" id="user_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Profile and settings">
-                    <img src={this.props.user_details.profile_picture} alt="Elmer Logo" style={{width: 36, height: 36, borderRadius: 50}} />
+                    <img src={this.state.user_details.profile_picture} alt="Elmer Logo" style={{width: 36, height: 36, borderRadius: 50}} />
                   </a>
                   <div className="dropdown-menu" aria-labelledby="user_dropdown" style={{right: 0, left: 'auto'}}>
-                    <Link className="dropdown-item font-weight-bold" to={`/u/${this.props.user_details.username}`}>{this.props.user_details.screen_name}</Link>
+                    <Link className="dropdown-item font-weight-bold" to={`/u/${this.state.user_details.username}`}>{this.state.user_details.screen_name}</Link>
                     <div className="dropdown-divider" />
-                    <Link className="dropdown-item" to={`/u/${this.props.user_details.username}`}>View profile</Link>
+                    <Link className="dropdown-item" to={`/u/${this.state.user_details.username}`}>View profile</Link>
                     <Link className="dropdown-item" to="/following">Following</Link>
                     <Link className="dropdown-item" to="/subscriptions">Subscriptions</Link>
                     <div className="dropdown-divider" />
@@ -52,7 +69,7 @@ class TopNavigation extends Component {
                     <div className="dropdown-divider" />
                     <Link className="dropdown-item" to="/settings">Account settings</Link>
                     <Link className="dropdown-item font-weight-light" to="feedback">Send Feedback</Link>
-                    <a className="dropdown-item" href="#" onClick={this.props.handle_logout}>Sign out</a>
+                    <a className="dropdown-item" href="#" onClick={() => this.props.logout()}>Sign out</a>
                   </div>
                 </div>
               </React.Fragment> :
@@ -68,4 +85,15 @@ class TopNavigation extends Component {
   }
 }
 
-export default TopNavigation;
+TopNavigation.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.token
+  };
+}
+
+export default connect(mapStateToProps, { logout: actions.logout })(TopNavigation);
