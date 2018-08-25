4ac3e6aaf16d6f92ace5f6f77b3f8c2287556e6d
Ali Saleh <alisheikh1114@gmail.com>
2018-08-26 01:40:34 +0500

    Issues fixed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,97 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import TopNavigation from './components/TopNavigation';
-import HomePage from './pages/HomePage';
-import TrendingPage from './pages/TrendingPage';
-import BoardsListPage from './pages/BoardsListPage';
-import BoardPage from './pages/BoardPage';
-import SubjectPage from './pages/SubjectPage';
-import SearchResultsPage from './pages/SearchResultsPage';
-import CreateSubjectPage from './pages/CreateSubjectPage';
-import CreateBoardPage from './pages/CreateBoardPage';
-import AboutPage from './pages/AboutPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import TermsOfUsePage from './pages/TermsOfUsePage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import UserRoute from './routes/UserRoute';
-import GuestRoute from './routes/GuestRoute';
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import TopNavigation from "./components/TopNavigation";
+import HomePage from "./pages/HomePage";
+import TrendingPage from "./pages/TrendingPage";
+import BoardsListPage from "./pages/BoardsListPage";
+import BoardPage from "./pages/BoardPage";
+import SubjectPage from "./pages/SubjectPage";
+import SearchResultsPage from "./pages/SearchResultsPage";
+import CreateSubjectPage from "./pages/CreateSubjectPage";
+import CreateBoardPage from "./pages/CreateBoardPage";
+import AboutPage from "./pages/AboutPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsOfUsePage from "./pages/TermsOfUsePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import UserRoute from "./routes/UserRoute";
+import GuestRoute from "./routes/GuestRoute";
 
 const App = ({ location, isAuthenticated }) => (
   <React.Fragment>
     <TopNavigation />
     <Switch>
-      <Route location={location} exact path='/' component={HomePage} />
-      <Route location={location} exact path='/trending' component={TrendingPage} />
-      <Route location={location} exact path='/boards' component={BoardsListPage} />
-      <Route location={location} exact path='/b/:board_slug' component={BoardPage} />
-      <Route location={location} exact path='/s/:board_slug/:subject_slug' component={SubjectPage} />
-      <Route location={location} exact path='/about' component={AboutPage} />
-      <Route location={location} exact path='/privacy' component={PrivacyPolicyPage} />
-      <Route location={location} exact path='/terms' component={TermsOfUsePage} />
-      <GuestRoute location={location} exact path='/login' component={LoginPage} />
-      <GuestRoute location={location} exact path='/signup' component={SignUpPage} />
-      <UserRoute location={location} exact path='/new_post' component={CreateSubjectPage} />
-      <UserRoute location={location} exact path='/new_board' component={CreateBoardPage} />
-      <Route location={location} exact path='/results' render={(props) => <SearchResultsPage {...props} />} />
+      <Route location={location} exact path="/" component={HomePage} />
+      <Route
+        location={location}
+        exact
+        path="/trending"
+        component={TrendingPage}
+      />
+      <Route
+        location={location}
+        exact
+        path="/boards"
+        component={BoardsListPage}
+      />
+      <Route
+        location={location}
+        exact
+        path="/b/:board_slug"
+        component={BoardPage}
+      />
+      <Route
+        location={location}
+        exact
+        path="/s/:board_slug/:subject_slug"
+        component={SubjectPage}
+      />
+      <Route location={location} exact path="/about" component={AboutPage} />
+      <Route
+        location={location}
+        exact
+        path="/privacy"
+        component={PrivacyPolicyPage}
+      />
+      <Route
+        location={location}
+        exact
+        path="/terms"
+        component={TermsOfUsePage}
+      />
+      <GuestRoute
+        location={location}
+        exact
+        path="/login"
+        component={LoginPage}
+      />
+      <GuestRoute
+        location={location}
+        exact
+        path="/signup"
+        component={SignUpPage}
+      />
+      <UserRoute
+        location={location}
+        exact
+        path="/new_post"
+        component={CreateSubjectPage}
+      />
+      <UserRoute
+        location={location}
+        exact
+        path="/new_board"
+        component={CreateBoardPage}
+      />
+      <Route
+        location={location}
+        exact
+        path="/results"
+        render={props => <SearchResultsPage {...props} />}
+      />
     </Switch>
   </React.Fragment>
 );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'jquery/dist/jquery.slim.min.js';
-import 'popper.js/dist/popper.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "jquery/dist/jquery.slim.min.js";
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import { userLoggedIn } from "./actions/auth";
-import setAuthorizationHeader from './utils/setAuthorizationHeader';
+import setAuthorizationHeader from "./utils/setAuthorizationHeader";
 
 if (localStorage.token) {
   const user = {
@@ -26,5 +26,7 @@ ReactDOM.render(
     <Provider store={store}>
       <Route component={App} />
     </Provider>
-  </Router>, document.getElementById('root'));
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
