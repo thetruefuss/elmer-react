27485d9b769b015ffa0ce5dc2e021f66a856c729
Ali Saleh <alisheikh1114@gmail.com>
2018-08-26 02:00:14 +0500

    Updated form to submit successfully without image

--- src/pages/CreateBoardPage.js
+++ src/pages/CreateBoardPage.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import axios from 'axios';
-import PageHeading from '../components/PageHeading';
-import CenteredFooter from '../components/CenteredFooter';
-import BoardForm from '../forms/BoardForm';
+import axios from "axios";
+import PageHeading from "../components/PageHeading";
+import CenteredFooter from "../components/CenteredFooter";
+import BoardForm from "../forms/BoardForm";
 
 class CreateBoardPage extends Component {
   componentDidMount() {
-    document.title = "New Board | Elmer"
+    document.title = "New Board | Elmer";
   }
 
   submit = data => {
     const formData = new FormData();
-    formData.append('title', data.title);
-    formData.append('description', data.description);
-    formData.append('cover', data.cover);
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    if (data.cover) {
+      formData.append("cover", data.cover);
+    }
     // Display the key/value pairs
     for (var pair of formData.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]);
+      console.log(pair[0] + ", " + pair[1]);
     }
-    axios.post("http://127.0.0.1:8000/api/frontboard/boards/create/", formData)
-    .then(res => this.goToBoard(res.data.slug));
+    axios
+      .post("http://127.0.0.1:8000/api/frontboard/boards/create/", formData)
+      .then(res => this.goToBoard(res.data.slug));
   };
 
   goToBoard = slug => this.props.history.push(`/b/${slug}`);
@@ -30,7 +33,10 @@ class CreateBoardPage extends Component {
       <React.Fragment>
         <div className="container form_content_block">
           <div className="row">
-            <div className="container" style={{margin: '0 auto', width: '50%'}}>
+            <div
+              className="container"
+              style={{ margin: "0 auto", width: "50%" }}
+            >
               <PageHeading text="Create new board" />
               <BoardForm submit={this.submit} />
               <CenteredFooter />
@@ -45,7 +51,7 @@ class CreateBoardPage extends Component {
 CreateBoardPage.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired
-  }).isRequired,
+  }).isRequired
 };
 
 export default CreateBoardPage;
--- src/pages/CreateSubjectPage.js
+++ src/pages/CreateSubjectPage.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import PageHeading from '../components/PageHeading';
-import CenteredFooter from '../components/CenteredFooter';
-import SubjectForm from '../forms/SubjectForm';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import PageHeading from "../components/PageHeading";
+import CenteredFooter from "../components/CenteredFooter";
+import SubjectForm from "../forms/SubjectForm";
 
 class CreateSubjectPage extends Component {
   componentDidMount() {
-    document.title = "New Subject | Elmer"
+    document.title = "New Subject | Elmer";
   }
 
   submit = data => {
     const formData = new FormData();
-    formData.append('title', data.title);
-    formData.append('body', data.body);
-    formData.append('photo', data.photo);
-    formData.append('board', data.board);
+    formData.append("title", data.title);
+    formData.append("body", data.body);
+    if (data.photo) {
+      formData.append("photo", data.photo);
+    }
+    formData.append("board", data.board);
     // Display the key/value pairs
     for (var pair of formData.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]);
+      console.log(pair[0] + ", " + pair[1]);
     }
-    axios.post("http://127.0.0.1:8000/api/frontboard/subjects/create/", formData)
-    .then(res => console.log(res))
-    .then(() => this.props.history.push("/"));
+    axios
+      .post("http://127.0.0.1:8000/api/frontboard/subjects/create/", formData)
+      .then(res => console.log(res))
+      .then(() => this.props.history.push("/"));
   };
 
   render() {
@@ -30,7 +33,10 @@ class CreateSubjectPage extends Component {
       <React.Fragment>
         <div className="container form_content_block">
           <div className="row">
-            <div className="container" style={{margin: '0 auto', width: '50%'}}>
+            <div
+              className="container"
+              style={{ margin: "0 auto", width: "50%" }}
+            >
               <PageHeading text="Compose a new post" />
               <SubjectForm submit={this.submit} />
               <CenteredFooter />
@@ -45,7 +51,7 @@ class CreateSubjectPage extends Component {
 CreateSubjectPage.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired
-  }).isRequired,
+  }).isRequired
 };
 
 export default CreateSubjectPage;
