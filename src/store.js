ccce5c736aa35b2c709a6f5609cb7666ba475469
Ali Saleh <alisheikh1114@gmail.com>
2018-08-29 15:26:00 +0500

    Updated store to use redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,18 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const initialState = {};
-
-const middleware = [thunk];
-
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
