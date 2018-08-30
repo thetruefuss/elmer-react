c4eec6b3330637cdeba40354d448cc7833bb8ada
Ali Saleh <alisheikh1114@gmail.com>
2018-08-30 20:06:09 +0500

    Added PropTypes

--- src/components/PageHeading.js
+++ src/components/PageHeading.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const PageHeading = ({ text }) => (
   <h1 className="pageHeadingH1">
     <span className="pageHeadingSpan">{text}</span>
   </h1>
 );
+
+PageHeading.propTypes = {
+  text: PropTypes.string.isRequired
+};
 
 export default PageHeading;
