6c7dfeb714c00451b0f976bbbdf10ae9403c2271
Ali Saleh <alisheikh1114@gmail.com>
2018-08-30 21:01:41 +0500

    Made it a pure component

--- src/components/BoardInfo.js
+++ src/components/BoardInfo.js
@@ -1,80 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class BoardInfo extends Component {
-  render() {
-    const { board_details } = this.props;
-
-    return (
-      <React.Fragment>
+const BoardInfo = ({ board_details }) => (
+  <React.Fragment>
+    <br />
+    <div className="card border-light">
+      <div
+        className="card-header"
+        style={{
+          color: "#607d8b",
+          borderBottom: "2px solid #1f89de",
+          display: "inline-block",
+          fontSize: 16,
+          fontWeight: 900,
+          letterSpacing: "-.4px",
+          margin: "3px 0",
+          paddingBottom: 0,
+          textTransform: "uppercase",
+          textAlign: "center"
+        }}>
+        Get updates now
         <br />
-        <div className="card border-light">
-          <div
-            className="card-header"
-            style={{
-              color: "#607d8b",
-              borderBottom: "2px solid #1f89de",
-              display: "inline-block",
-              fontSize: 16,
-              fontWeight: 900,
-              letterSpacing: "-.4px",
-              margin: "3px 0",
-              paddingBottom: 0,
-              textTransform: "uppercase",
-              textAlign: "center"
-            }}>
-            Get updates now
-            <br />
-            {board_details.is_subscribed === true ? (
-              <a
-                href="/"
-                className="btn btn-secondary btn-sm"
-                id="subscribe_board"
-                style={{ marginBottom: 10 }}>
-                Unsubscribe
-              </a>
-            ) : (
-              <a
-                href="/"
-                className="btn btn-primary btn-sm"
-                id="subscribe_board"
-                style={{ marginBottom: 10 }}>
-                Subscribe
-              </a>
-            )}
-          </div>
-          <ul className="list-group list-group-flush" style={{ fontSize: 14 }}>
-            <li className="list-group-item text-center text-muted">
-              Board Info
-            </li>
-            <li className="list-group-item">
-              <b>Title: </b>
-              {board_details.title}
-            </li>
-            <li className="list-group-item">
-              <b>Description: </b>
-              {board_details.description}
-            </li>
-            <li className="list-group-item">
-              <b>Total Posts: </b>
-              {board_details.total_posts}
-            </li>
-            <li className="list-group-item">
-              <b>Admins: </b>
-              Add Later
-            </li>
-            <li className="list-group-item">
-              <b>Subscribers: </b>
-              <span id="board_subscribers">
-                {board_details.subscribers_count}
-              </span>
-            </li>
-          </ul>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+        {board_details.is_subscribed === true ? (
+          <a
+            href="/"
+            className="btn btn-secondary btn-sm"
+            id="subscribe_board"
+            style={{ marginBottom: 10 }}>
+            Unsubscribe
+          </a>
+        ) : (
+          <a
+            href="/"
+            className="btn btn-primary btn-sm"
+            id="subscribe_board"
+            style={{ marginBottom: 10 }}>
+            Subscribe
+          </a>
+        )}
+      </div>
+      <ul className="list-group list-group-flush" style={{ fontSize: 14 }}>
+        <li className="list-group-item text-center text-muted">Board Info</li>
+        <li className="list-group-item">
+          <b>Title: </b>
+          {board_details.title}
+        </li>
+        <li className="list-group-item">
+          <b>Description: </b>
+          {board_details.description}
+        </li>
+        <li className="list-group-item">
+          <b>Total Posts: </b>
+          {board_details.total_posts}
+        </li>
+        <li className="list-group-item">
+          <b>Admins: </b>
+          Add Later
+        </li>
+        <li className="list-group-item">
+          <b>Subscribers: </b>
+          <span id="board_subscribers">{board_details.subscribers_count}</span>
+        </li>
+      </ul>
+    </div>
+  </React.Fragment>
+);
 
 BoardInfo.propTypes = {
   board_details: PropTypes.shape({
