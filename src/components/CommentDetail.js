e04751b70506fc151e27e7950eecba4d18beb180
Ali Saleh <alisheikh1114@gmail.com>
2018-08-30 20:58:45 +0500

    Added prop-types & made it function based component

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,60 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Linkify from "react-linkify";
 
-class CommentDetail extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      comment: this.props.comment
-    };
-  }
-
-  render() {
-    const { comment } = this.state;
-    return (
-      <li className="list-group-item">
-        <p>
-          <Linkify>{comment.body}</Linkify>
-          <br />
-          <Link
-            to={`/u/${comment.commenter.username}`}
-            title="view profile"
-            id="commenter"
-            className="card-link">
-            {comment.commenter.screen_name}
-          </Link>{" "}
-          &#8212; <span>{comment.created_naturaltime}</span>{" "}
-          {comment.is_commenter ? (
-            <a href="/" title="delete this comment" id="delete_comment">
-              <i className="fa fa-trash-o" />
-            </a>
-          ) : (
-            <React.Fragment>
-              <a
-                href="/"
-                style={{ textDecoration: "none" }}
-                title="reply to this comment"
-                id="reply_comment">
-                <i className="fa fa-reply" /> Reply
-              </a>{" "}
-              &bull;
-              <a
-                href="/"
-                style={{ textDecoration: "none" }}
-                title="report this comment"
-                id="report_link">
-                <i className="fa fa-flag-o" /> Report
-              </a>
-            </React.Fragment>
-          )}
-        </p>
-      </li>
-    );
-  }
-}
+const CommentDetail = ({ comment }) => (
+  <li className="list-group-item">
+    <p>
+      <Linkify>{comment.body}</Linkify>
+      <br />
+      <Link
+        to={`/u/${comment.commenter.username}`}
+        title="view profile"
+        id="commenter"
+        className="card-link">
+        {comment.commenter.screen_name}
+      </Link>{" "}
+      &#8212; <span>{comment.created_naturaltime}</span>{" "}
+      {comment.is_commenter ? (
+        <a href="/" title="delete this comment" id="delete_comment">
+          <i className="fa fa-trash-o" />
+        </a>
+      ) : (
+        <React.Fragment>
+          <a
+            href="/"
+            style={{ textDecoration: "none" }}
+            title="reply to this comment"
+            id="reply_comment">
+            <i className="fa fa-reply" /> Reply
+          </a>{" "}
+          &bull;
+          <a
+            href="/"
+            style={{ textDecoration: "none" }}
+            title="report this comment"
+            id="report_link">
+            <i className="fa fa-flag-o" /> Report
+          </a>
+        </React.Fragment>
+      )}
+    </p>
+  </li>
+);
 
 CommentDetail.propTypes = {
   comment: PropTypes.shape({
@@ -64,8 +51,7 @@ CommentDetail.propTypes = {
       username: PropTypes.string.isRequired
     }).isRequired,
     is_commenter: PropTypes.bool.isRequired,
-    created: PropTypes.string.isRequired,
-    created_naturaltime: PropTypes.string.isRequired,
+    created_naturaltime: PropTypes.string.isRequired
   }).isRequired
 };
 
