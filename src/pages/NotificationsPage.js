d21e2c3b63bdca08ef742f4c38bd9e105a7d0cc0
Ali Saleh <alisheikh1114@gmail.com>
2018-09-01 16:34:14 +0500

    Added notifications page

---
+++ src/pages/NotificationsPage.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PageHeading from "../components/PageHeading";
+import TrendingBoards from "../components/TrendingBoards";
+import ActiveThreads from "../components/ActiveThreads";
+import NotificationsList from "../components/NotificationsList";
+import Footer from "../components/Footer";
+
+class NotificationsPage extends Component {
+  componentDidMount() {
+    document.title = "Elmer - A social network inspired by reddit";
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container content_block">
+          <div className="row">
+            <div className="col-lg-8 col-md-8">
+              <PageHeading text="Notifications" />
+              <NotificationsList />
+            </div>
+
+            <div class="col-lg-4 col-md-4">
+              <Link
+                to="/new_post"
+                className="btn btn-primary btn-block mt-4"
+                role="button"
+                title="Create new post"
+                style={{ fontWeight: "bold", letterSpacing: "0.8px" }}>
+                CREATE NEW POST
+              </Link>
+              <ActiveThreads />
+              <TrendingBoards />
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+export default NotificationsPage;
